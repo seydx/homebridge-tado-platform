4a0e8983e4bedea8743bf9396f5c8196c906df5e
Seyd <34152761+SeydX@users.noreply.github.com>
2019-04-25 10:51:01 +0200

    Update boiler.js

--- src/accessories/boiler.js
+++ src/accessories/boiler.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const timeout = ms => new Promise(res => setTimeout(res, ms));
-
 var Service, Characteristic;
 
 class thermostat_Accessory {
@@ -24,90 +22,159 @@ class thermostat_Accessory {
     this.tadoHandler = platform.tadoHandler;
     
     this.settedState = 0;
+
+    this.accessory = accessory;
+    this.mainService = this.accessory.getService(Service.Faucet, this.accessory.displayName);
     
-    this.getService(accessory);
+    this.handleValve();
+    
+    this.getService();
   
   }
 
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
   // Services
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+  
+  handleValve(){
+ 
+    if(!this.accessory.getServiceByUUIDAndSubType(Service.Valve, 'Hot Water Valve')){
+   
+      this.valveService = new Service.Valve('Hot Water', 'Hot Water Valve');
+      
+      this.valveService.addCharacteristic(Characteristic.ConfiguredName);
+  
+      this.valveService
+        .setCharacteristic(Characteristic.Name, 'Hot Water')
+        .setCharacteristic(Characteristic.ServiceLabelIndex, 1)
+        .setCharacteristic(Characteristic.ConfiguredName, 'Hot Water')
+        .setCharacteristic(Characteristic.IsConfigured, Characteristic.IsConfigured.CONFIGURED)
+        .setCharacteristic(Characteristic.ValveType, Characteristic.ValveType.WATER_FAUCET);
+        
+      this.accessory.addService(this.valveService);
+      this.mainService.addLinkedService(this.valveService);
+            
+    } else {
+  
+      this.valveService = this.accessory.getServiceByUUIDAndSubType(Service.Valve, 'Hot Water Valve');  
+      this.mainService.addLinkedService(this.valveService);
+        
+    }
+    
+    this.valveService.getCharacteristic(Characteristic.Active)
+      .on('set', function(state, callback){
+        callback();
+      });
+      
+    this.valveService.getCharacteristic(Characteristic.InUse)
+      .on('set', function(state, callback){
+        callback();
+      });
+        
+  }
 
-  getService (accessory) {
+  getService() {
   
     const self = this;
 
-    accessory.on('identify', function(paired, callback) {
-      self.logger.info(accessory.displayName + ': Identify!!!');
+    this.accessory.on('identify', function(paired, callback) {
+      self.logger.info(self.accessory.displayName + ': Identify!!!');
       callback();
     });
     
-    let service = accessory.getService(Service.Valve);
-        
-    service.setCharacteristic(Characteristic.ValveType, Characteristic.ValveType.WATER_FAUCET);
+    this.mainService.getCharacteristic(Characteristic.Active)
+      .on('set', this.setState.bind(this));
+      
+    if(this.accessory.context.canSetTemperature){
     
-    service.getCharacteristic(Characteristic.Active)
-      .on('set', this.setState.bind(this, accessory, service));
+      this.accessory.context.cachedTemp = this.accessory.context.cachedTemp||this.accessory.context.minValue;
+      
+      if(!this.mainService.testCharacteristic(Characteristic.CurrentTemperature))
+        this.mainService.addCharacteristic(Characteristic.CurrentTemperature);
+      
+      this.mainService.getCharacteristic(Characteristic.CurrentTemperature)
+        .setProps({ maxValue: this.accessory.context.maxValue, minValue: this.accessory.context.minValue, minStep: 1 });
+      
+      if(!this.mainService.testCharacteristic(Characteristic.TargetHeaterCoolerState))
+        this.mainService.addCharacteristic(Characteristic.TargetHeaterCoolerState);
+      
+      this.mainService.getCharacteristic(Characteristic.TargetHeaterCoolerState)
+        .setProps({ maxValue: 1, minValue: 1, validValues: [1] })
+        .updateValue(1);
+      
+      if(!this.mainService.testCharacteristic(Characteristic.HeatingThresholdTemperature))
+        this.mainService.addCharacteristic(Characteristic.HeatingThresholdTemperature);
+      
+      this.mainService.getCharacteristic(Characteristic.HeatingThresholdTemperature)
+        .setProps({ maxValue: this.accessory.context.maxValue, minValue: this.accessory.context.minValue, minStep: 1 })
+        .on('set', this.setTemp.bind(this))
+        .updateValue(this.accessory.context.cachedTemp);
     
-    this.getState(accessory, service);
+    }
+    
+    this.getState();
 
   }
   
-  async getState (accessory, service){
+  async getState (){
   
     try {
     
-      let getSet = await this.getSetStatus();
-    
-      let zone = await this.tadoHandler.getZone(accessory.context.zoneID);
+      let zone = await this.tadoHandler.getZone(this.accessory.context.zoneID);
       
       if(zone.setting.power === 'OFF') {
         
-        if(getSet){
-          service.getCharacteristic(Characteristic.InUse).updateValue(0);
-          service.getCharacteristic(Characteristic.Active).updateValue(0);  
-        }
+        this.valveService.getCharacteristic(Characteristic.InUse).updateValue(0);
+        this.valveService.getCharacteristic(Characteristic.Active).updateValue(0); 
+        this.mainService.getCharacteristic(Characteristic.Active).updateValue(0); 
      
       } else {
-       
-        if(getSet){
-          service.getCharacteristic(Characteristic.Active).updateValue(1);
-          service.getCharacteristic(Characteristic.InUse).updateValue(1);  
+
+        this.mainService.getCharacteristic(Characteristic.Active).updateValue(1); 
+        this.valveService.getCharacteristic(Characteristic.Active).updateValue(1); 
+        this.valveService.getCharacteristic(Characteristic.InUse).updateValue(1);
+        
+        if(this.accessory.context.canSetTemperature){
+          
+          this.accessory.context.cachedTemp = ( this.accessory.context.unit === 1 ) ? zone.setting.temperature.fahrenheit : zone.setting.temperature.celsius;
+      
+          this.mainService.getCharacteristic(Characteristic.HeatingThresholdTemperature)
+            .updateValue(this.accessory.context.cachedTemp); 
+        
         }
         
       }
     
     } catch(err) {
     
-      this.debug(accessory.displayName + ': An error occured while getting new state');
+      this.debug(this.accessory.displayName + ': An error occured while getting new state');
       this.debug(err);
     
     } finally {
   
-      if(!accessory.context.remove) setTimeout(this.getState.bind(this,accessory,service),accessory.context.polling);
+      if(!this.accessory.context.remove) setTimeout(this.getState.bind(this), this.accessory.context.polling);
       
     }
   
   }
   
-  async setState(accessory,service,state,callback){
+  async setState(state,callback){
   
     let failed;
   
     try {
     
-      this.settedState++;
-    
-      this.logger.info(accessory.displayName + ': ' + (state ? 'On' : 'Off'));
+      this.logger.info(this.accessory.displayName + ': ' + (state ? 'On' : 'Off'));
       
-      let termination = state ? accessory.context.overrideMode : 'manual';
+      let termination = state ? this.accessory.context.overrideMode : 'manual';
       let onOff = state ? 'on' : 'off';
+      let temp = ( this.accessory.context.canSetTemperature && state ) ? this.valveService.getCharacteristic(Characteristic.HeatingThresholdTemperature).value : null;
         
-      await this.tado.setZoneOverlay(accessory.context.homeID,accessory.context.zoneID,onOff,null,termination,accessory.context.zoneType);
+      await this.tado.setZoneOverlay(this.accessory.context.homeID,this.accessory.context.zoneID,onOff,temp,termination,this.accessory.context.zoneType);
 
     } catch(err) {
     
-      this.logger.error(accessory.displayName + ': An error occured while setting new state!'); 
+      this.logger.error(this.accessory.displayName + ': An error occured while setting new state!'); 
       this.debug(err);
       
       failed = err;
@@ -119,13 +186,15 @@ class thermostat_Accessory {
       
       if(state){
       
-        service.getCharacteristic(Characteristic.Active).updateValue(1);
-        service.getCharacteristic(Characteristic.InUse).updateValue(1);  
+        this.valveService.getCharacteristic(Characteristic.InUse).updateValue(0);
+        this.valveService.getCharacteristic(Characteristic.Active).updateValue(0); 
+        this.mainService.getCharacteristic(Characteristic.Active).updateValue(0); 
       
       } else {
       
-        service.getCharacteristic(Characteristic.InUse).updateValue(0);
-        service.getCharacteristic(Characteristic.Active).updateValue(0);  
+        this.mainService.getCharacteristic(Characteristic.Active).updateValue(1); 
+        this.valveService.getCharacteristic(Characteristic.Active).updateValue(1); 
+        this.valveService.getCharacteristic(Characteristic.InUse).updateValue(1);
       
       }
       
@@ -135,31 +204,29 @@ class thermostat_Accessory {
     
   }
   
-  async getSetStatus(){
-  
-    if(this.settedState){
+  async setTemp(value, callback){
     
-      this.oldState = this.settedState; 
+    try {
     
-      await timeout(5000);
-    
-      if(this.settedState === this.oldState){
-    
-        this.settedState = 0;
-        return true;
-    
-      } else {
-    
-        return false;
-    
+      if(this.mainService.getCharacteristic(Characteristic.Active).value){
+      
+        this.logger.info(this.accessory.displayName + ': Setting new temperature: ' + value);
+
+        await this.tado.setZoneOverlay(this.accessory.context.homeID,this.accessory.context.zoneID,'on',value,this.accessory.context.overrideMode,this.accessory.context.zoneType,this.accessory.context.unit);
+      
       }
+
+    } catch(err) {
     
-    } else {
+      this.logger.error(this.accessory.displayName + ': An error occured while setting new temp!'); 
+      this.debug(err);
     
-      return true;
+    } finally {
+      
+      callback();
     
     }
-  
+
   }
 
 }
